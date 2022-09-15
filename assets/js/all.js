2ec78797bac0f12698fbbe34e505593cef916f41
Eric <erickaivalue@gmail.com>
2022-09-15 11:55:25 +0800

    Update 2022-09-15T03:55:23.415Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -1,6 +1,30 @@
 "use strict";
 
-// Swiper
+var gridMasonry = document.querySelector(".grid-masonry");
+
+function masonry() {
+  var msnry = new Masonry(gridMasonry, {
+    itemSelector: ".grid-masonry-item"
+  });
+  imagesLoaded(gridMasonry).on("progress", function () {
+    msnry.layout();
+  });
+}
+
+if (gridMasonry) {
+  window.addEventListener("load", function () {
+    masonry();
+  });
+} // 解決瀑布流 tab 切換取不到高度
+
+
+var dataTabs = document.querySelectorAll("button[data-bs-toggle=tab]");
+dataTabs.forEach(function (dataTab) {
+  dataTab.addEventListener("shown.bs.tab", function () {
+    masonry();
+  });
+}); // Swiper
+
 var swiper = new Swiper(".swiper", {
   slidesPerView: 1,
   spaceBetween: 10,
@@ -26,8 +50,8 @@ var swiper = new Swiper(".swiper", {
     }
   }
 }); // 確保圖片都載入後，以免造成剛進入頁面時圖片會被覆蓋
-
-$('.row').imagesLoaded().progress(function () {// 渲染整體畫面
-});
-$('.row').masonry();
+// $('.row').imagesLoaded().progress( function() {
+// // 渲染整體畫面
+// });
+// $('.row').masonry();
 //# sourceMappingURL=all.js.map
